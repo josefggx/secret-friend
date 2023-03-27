c9dea211212c3c3bf1164eadf8981fbe5403ad7e
josefggx <josefgg58@gmail.com>
2023-03-27 18:49:38 -0500

    Refactor done

--- app/services/game_creator.rb
+++ app/services/game_creator.rb
@@ -4,8 +4,8 @@ class GameCreator
   def initialize(game_params)
     @game_params = game_params
     @workers_already_coupled = []
-    @available_workers = find_available_workers
-    @available_workers_ids = @available_workers.map(&:id)
+    @all_workers = find_and_order_all_workers
+    @all_workers_ids = @all_workers.map(&:id)
   end
 
   def create_game
@@ -19,49 +19,47 @@ class GameCreator
 
   attr_reader :game_params
 
-  def find_available_workers
-    workers_without_play_ids = [last_worker_without_play_id, next_worker_without_play_id]
+  def find_and_order_all_workers
+    workers_without_play_ids = [last_without_play_id, next_without_play_id]
     Worker.order_by_without_play_first(workers_without_play_ids).all
   end
 
   def generate_all_couples(game)
-    @available_workers.each do |worker|
+    @all_workers.each do |worker|
       next if @workers_already_coupled.include?(worker.id)
 
-      couple_options = find_couple_options(worker)
+      pairing_options = find_pairing_options(worker)
 
-      if couple_options.any?
-        generate_couple_for_worker(worker, game, couple_options)
-      else
-        game.worker_without_play_id = worker.id
-      end
+      pairing_options.any? ? generate_couple(worker, pairing_options, game) : assign_without_play(game, worker)
     end
   end
 
-  def generate_couple_for_worker(worker, game, couple_options)
-    couple_pick = pick_partner(couple_options)
-    @workers_already_coupled.push(worker.id, couple_pick.id)
-    couple = Couple.create(first_worker: worker, second_worker: couple_pick, game_id: game.id)
+  def find_pairing_options(worker)
+    @all_workers_ids - ([worker.id] + @workers_already_coupled + worker.find_restricted_partners_ids(actual_year))
+  end
+
+  def assign_without_play(game, worker)
+    game.worker_without_play = worker
+  end
+
+  def generate_couple(worker, pairing_options, game)
+    partner = pick_partner(pairing_options)
+    @workers_already_coupled.push(worker.id, partner.id)
+    couple = Couple.create(first_worker: worker, second_worker: partner, game_id: game.id)
     game.couples << couple
   end
 
   def pick_partner(couple_options)
-    if next_worker_without_play_id.in?(couple_options)
-      @available_workers.find(next_worker_without_play_id)
-    else
-      @available_workers.find(couple_options.sample)
-    end
+    return @all_workers.find(next_without_play_id) if next_without_play_id.in?(couple_options)
+
+    @all_workers.find(couple_options.sample)
   end
 
-  def find_couple_options(worker)
-    @available_workers_ids - ([worker.id] + @workers_already_coupled + worker.find_restricted_partners_ids(actual_year))
-  end
-
-  def next_worker_without_play_id
+  def next_without_play_id
     Game.where(year_game: actual_year + 1).first&.worker_without_play_id.to_i
   end
 
-  def last_worker_without_play_id
+  def last_without_play_id
     Game.where(year_game: actual_year - 1).first&.worker_without_play_id.to_i
   end
 
